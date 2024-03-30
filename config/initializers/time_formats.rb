417eab97bf4cc760b68ef502ba08cf9c9ef38eec
nakamoto3211 <tkrs6728@gmail.com>
2024-03-30 19:39:04 +0900

    ログイン認証の範囲設定

--- config/initializers/time_formats.rb
+++
@@ -1,3 +0,0 @@
-Time::DATE_FORMATS[:date_jp] = '%Y/%m/%d'
-Time::DATE_FORMATS[:datetime_jp] = '%Y/%m/%d %H:%M'
-Date::DATE_FORMATS[:date_jp] = '%Y/%m/%d'
--- db/migrate/20240210015109_add_room_id_to_reservation.rb
+++
@@ -1,5 +0,0 @@
-class AddRoomIdToReservation < ActiveRecord::Migration[6.1]
-  def change
-    add_column :reservations, :room_id, :integer
-  end
-end
